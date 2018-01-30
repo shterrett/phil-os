a4ebd6ab6c4ae349f7e5f4ee53f61fae152a3bd6
Stuart Terrett <shterrett@gmail.com>
2018-01-29 21:19:55 -0500

    Link rust code

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -1,15 +1,19 @@
 global long_mode_start
+extern rust_main
 
 section .text
 bits 64
 long_mode_start:
   ; load 0 into all data segment registers
-    mov ax, 0
-    mov ss, ax
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
+  mov ax, 0
+  mov ss, ax
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+
+  extern rust_main
+  call rust_main
 
   ; print OKAY to the screen
   mov rax, 0x2f592f412f4b2f4f

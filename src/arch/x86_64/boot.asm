68ede0fe411932b56ae6ed79f5faefdbcba98db1
Stuart Terrett <shterrett@gmail.com>
2018-02-04 13:47:48 -0500

    Get memory information

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -5,6 +5,7 @@ section .text
 bits 32
 start:
   mov esp, stack_top
+  mov edi, ebx ; move multiboot info pointer to edi
 
   call check_multiboot
   call check_cpuid
@@ -161,5 +162,5 @@ p3_table:
 p2_table:
   resb 4096
 stack_bottom:
-  resb 64
+  resb 4096 * 4
 stack_top:
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@
 extern crate rlibc;
 extern crate volatile;
 extern crate spin;
+extern crate multiboot2;
 
 #[macro_use]
 mod vga_buffer;
@@ -14,12 +15,42 @@ mod vga_buffer;
 use core::fmt::Write;
 
 #[no_mangle]
-pub extern fn rust_main() {
-    vga_buffer::clear_screen();
-    println!("Hello World{}", "!");
-    println!("{}", { println!("inner"); "outer" });
+pub extern fn rust_main(multiboot_information_address: usize) {
+    let boot_info = unsafe { multiboot2::load(multiboot_information_address) };
+    let memory_map_tag = boot_info.memory_map_tag()
+                                  .expect("Memory map tag required");
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!("    start: 0x{:x}, length: 0x{:x}",
+                    area.base_addr, area.length);
+    }
+
+    let elf_sections_tag = boot_info.elf_sections_tag()
+                                    .expect("Elf-sections tag required");
+
+    println!("kernel sections:");
+    for section in elf_sections_tag.sections() {
+        println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
+                 section.addr, section.size, section.flags);
+    }
+
+    let kernel_start = elf_sections_tag.sections().map(|s| s.addr).min().unwrap();
+    let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size).max().unwrap();
+    let multiboot_start = multiboot_information_address;
+    let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+
+    println!("kernel_start: 0x{:x}, kernel_end: 0x{:x}", kernel_start, kernel_end);
+    println!("multiboot_start: 0x{:x}, multiboot_end: 0x{:x}", multiboot_start, multiboot_end);
+
     loop {};
 }
 
 #[lang = "eh_personality"] #[no_mangle] pub extern fn eh_personality() {}
-#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
+    loop{}
+}

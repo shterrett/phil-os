9da54cf14d4b7ddf41fc47827eb17877b71a8e50
Stuart Terrett <shterrett@gmail.com>
2018-02-06 21:45:10 -0500

    Walk page table tree

--- src/memory/paging/table.rs
+++ src/memory/paging/table.rs
@@ -1,12 +1,41 @@
 use core::ops::{ Index, IndexMut };
+use core::marker::PhantomData;
 use memory::paging::entry::*;
 use memory::paging::ENTRY_COUNT;
 
-pub struct Table {
-    entries: [Entry; ENTRY_COUNT]
+pub const P4: *mut Table<Level4> = 0xffffffff_fffff000 as *mut _;
+
+pub trait TableLevel {}
+pub enum Level4 {}
+pub enum Level3 {}
+pub enum Level2 {}
+pub enum Level1 {}
+
+impl TableLevel for Level4 {}
+impl TableLevel for Level3 {}
+impl TableLevel for Level2 {}
+impl TableLevel for Level1 {}
+
+pub trait HierarchialLevel: TableLevel {
+    type NextLevel: TableLevel;
 }
 
-impl Index<usize> for Table {
+impl HierarchialLevel for Level4 {
+    type NextLevel = Level3;
+}
+impl HierarchialLevel for Level3 {
+    type NextLevel = Level2;
+}
+impl HierarchialLevel for Level2 {
+    type NextLevel = Level1;
+}
+
+pub struct Table<L: TableLevel> {
+    entries: [Entry; ENTRY_COUNT],
+    level: PhantomData<L>
+}
+
+impl<L> Index<usize> for Table<L> where L: TableLevel {
     type Output = Entry;
 
     fn index(&self, index: usize) -> &Entry {
@@ -14,16 +43,38 @@ impl Index<usize> for Table {
     }
 }
 
-impl IndexMut<usize> for Table {
+impl<L> IndexMut<usize> for Table<L> where L: TableLevel {
     fn index_mut(&mut self, index: usize) -> &mut Entry {
         &mut self.entries[index]
     }
 }
 
-impl Table {
+impl<L> Table<L> where L: TableLevel {
     pub fn zero(&mut self) {
         for entry in self.entries.iter_mut() {
             entry.set_unused();
         }
+    }
+}
+
+impl<L> Table<L> where L: HierarchialLevel {
+    fn next_table_address(&self, index: usize) -> Option<usize> {
+        let entry_flags = self[index].flags();
+        if entry_flags.contains(PRESENT) && !entry_flags.contains(HUGE_PAGE) {
+            let table_address = self as *const _ as usize;
+            Some((table_address << 9) | (index << 12))
+        } else {
+            None
+        }
+    }
+
+    pub fn next_table(&self, index: usize) -> Option<&Table<L::NextLevel>> {
+        self.next_table_address(index)
+            .map(|addr| unsafe { &*(addr as *const _) })
+    }
+
+    pub fn next_table_mut(&mut self, index: usize) -> Option<&mut Table<L::NextLevel>> {
+        self.next_table_address(index)
+            .map(|addr| unsafe { &mut *(addr as *mut _) })
     }
 }

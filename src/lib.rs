19a408e6ba430e882b378860433f00c4380a3d5b
Stuart Terrett <shterrett@gmail.com>
2018-02-15 07:54:43 -0500

    Use linked-list allocator

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ extern crate x86_64;
 extern crate alloc;
 #[macro_use]
 extern crate once;
+extern crate linked_list_allocator;
 
 #[macro_use]
 mod vga_buffer;
@@ -27,12 +28,13 @@ mod memory;
 use memory::area_frame_allocator::AreaFrameAllocator;
 use memory::FrameAllocator;
 use memory::BumpAllocator;
+use linked_list_allocator::LockedHeap;
 
 pub const HEAP_START: usize = 0o_000_001_000_000_0000;
 pub const HEAP_SIZE: usize = 100 * 1024;
 
 #[global_allocator]
-static HEAP_ALLOCATOR: BumpAllocator = BumpAllocator::new(HEAP_START, HEAP_START + HEAP_SIZE);
+static HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 #[no_mangle]
 pub extern "C" fn rust_main(multiboot_information_address: usize) {
@@ -48,8 +50,16 @@ pub extern "C" fn rust_main(multiboot_information_address: usize) {
 
     memory::init(boot_info);
 
+    unsafe {
+        HEAP_ALLOCATOR.lock().init(HEAP_START, HEAP_START + HEAP_SIZE);
+    };
+
     use alloc::boxed::Box;
     let heap_test = Box::new(42);
+
+    for i in 0..10000 {
+        format!("Some String");
+    }
 
     println!("It did not crash!");
 
